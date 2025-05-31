42d21aa672d964ebb1c0376b1fcee10d1dc9f9ba
hoaC0 <hoachau1010@gmail.com>
2025-05-31 19:20:24 +0200

    Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+
+// Komponenten muessen importiert werden um verwenden werden zu koennen ( wie React )
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent]
 })
 
 export class AppComponent {
