19a5ca3aa53ad157e2a9096523f918eabf314c8d
hoa <hoachau1010@gmail.com>
2025-06-10 19:12:06 +0200

    Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { TaskComponent } from "./components/task/task.component";
 
 // Komponenten muessen importiert werden um verwenden werden zu koennen ( wie React )
 
@@ -8,7 +9,7 @@ import { HeaderComponent } from "./components/header/header.component";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, TaskComponent]
 })
 
 export class AppComponent {
