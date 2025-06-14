310870f0cfad743298b09cbd451cfb60dcfd881c
hoa <hoachau1010@gmail.com>
2025-06-14 19:31:02 +0200

    Upload

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -2,6 +2,10 @@ import { Task } from './Task';
 
 export const TASKS: Task[] = [
     {
-
+        id: 1,
+        title: 'Homework',
+        text: 'Complete the math assignment',
+        date: '2023-10-01',
+        reminder: true
     }
 ]
