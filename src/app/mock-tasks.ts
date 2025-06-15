bbd82dc4deb814188a376ae15216076118834d23
hoa <hoachau1010@gmail.com>
2025-06-15 22:28:06 +0200

    Upload

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -5,21 +5,21 @@ export const TASKS: Task[] = [
         id: 1,
         title: 'Homework',
         text: 'Complete the math assignment',
-        date: '2025-10-01',
+        date: '10.10.2025',
         reminder: true
     },
     {
         id: 2,
         title: 'Grocery Shopping',
         text: 'Buy vegetables and fruits',
-        date: '2025-10-02',
+        date: '25.10.2025',
         reminder: false
     },
     {
         id: 3,
         title: 'Gym',
         text: 'Attend the evening yoga class',
-        date: '2025-10-03',
+        date: '03.11.2025',
         reminder: true
     }
 ]
